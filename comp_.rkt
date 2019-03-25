d4e811a08d29bf9394fc3c8e55d2be1f5a2068eb
DexterLagan <38226103+DexterLagan@users.noreply.github.com>
2019-03-25 15:11:56 +0100

    Initial upload

---
+++ comp_.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(module+ test (require rackunit))
+
+;;; purpose
+
+; an in-order variant of ComposeX
+; Example:
+; ((comp_ (string-append "1" _ "3")
+;         (string-append _ "end"))
+;    "2")
+; -> "123end"
+
+;;; defs
+
+; returns a function that composes parameters in order,
+; using a placeholder _ for passing values between functions.
+(define-syntax (comp_ stx)
+  ; macro to compose functions passing an '_' parameter
+  (syntax-case stx ()
+    ((_ f1 ...)
+     (with-syntax ([x-var (datum->syntax stx '_)])
+       #'(apply compose1 (reverse (list (λ (x-var) f1) ...)))))))
+; unit test
+(module+ test
+  (check-equal? ((comp_ (string-trim _)
+                        (string-downcase _)
+                        (string-replace _ " " "-")
+                        ) "Hello World")
+                "hello-world"))
